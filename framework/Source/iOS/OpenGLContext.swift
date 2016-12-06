daac471700eaa5b6408659a2c27435e51bb32553
Robin Chou <chourobin@gmail.com>
2016-12-06 17:22:26 -0500

    Expose `-supportsTextureCaches`.

--- framework/Source/iOS/OpenGLContext.swift
+++ framework/Source/iOS/OpenGLContext.swift
@@ -61,7 +61,7 @@ public class OpenGLContext: SerialDispatch {
     // MARK: -
     // MARK: Device capabilities
     
-    func supportsTextureCaches() -> Bool {
+    public func supportsTextureCaches() -> Bool {
 #if (arch(i386) || arch(x86_64)) && os(iOS)
         return false // Simulator glitches out on use of texture caches
 #else
